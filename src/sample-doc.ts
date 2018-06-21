4a75d25d8a45a69fd8a765ddff012744c1b69fe2
Rajasekar <relango@salesforce.com>
2018-06-21 18:55:25 -0400

    Initial version of asciidoc with hljs

--- src/sample-doc.ts
+++ src/sample-doc.ts
@@ -3,10 +3,20 @@
 export namespace SampleDoc {
     export function convert() {
        var asciidoctor = require('asciidoctor.js')();
-        var content = "http://asciidoctor.org[*Asciidoctor*] " +
-            "running on https://opalrb.com[_Opal_] " +
-            "brings AsciiDoc to Node.js!";
-        var html = asciidoctor.convert(content);
+     //   var content = ":source-highlighter: highlightjs\n" +
+     var content = "" + 
+        "[source,ruby]\n" +
+        ".app.rb\n" +
+        "----\n" +
+        "require 'sinatra'\n" +
+        "get '/hi' do # <1> \n" +
+        " \"Hello World!\"\n" +
+        "end\n" +
+        "----\n" + 
+        "<1> test comment";
+        var html = asciidoctor.convert(content, {attributes: {'source-highlighter': 'highlightjs', showtitle: true, icons: 'font'}});
+     //   var html = asciidoctor.convert(content);
+       
         console.log(html);
     }
 }
